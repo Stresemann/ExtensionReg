d6ce235859c976a24ff5b071d4c0d9d8bd40b4f1
Preston Taukulis <prestontaukulis@Prestons-MacBook-Pro.local>
2019-01-02 19:30:13 -0500

    Added pluging registration

--- src/main/java/com/silvereaglesystems/mvc/GreetingsController.java
+++ src/main/java/com/silvereaglesystems/mvc/GreetingsController.java
@@ -1,4 +1,8 @@
 package com.silvereaglesystems.mvc;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.MediaType;
@@ -7,13 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.vmware.automatic.plugin.registration.PluginRegistrationMain;
+import com.vmware.automatic.plugin.registration.commands.IsPluginRegisteredCmd;
+import com.vmware.automatic.plugin.registration.commands.PluginCmd;
+import com.vmware.automatic.plugin.registration.commands.RegisterPluginCmd;
+import com.vmware.automatic.plugin.registration.commands.UnregisterPluginCmd;
+import com.vmware.automatic.plugin.registration.commands.UpdatePluginCmd;
 import com.silvereaglesystems.mvc.models.Client;
 import com.silvereaglesystems.mvc.models.Description;
 import com.silvereaglesystems.mvc.models.Extension;
 
 @RestController
 public class GreetingsController {
+	private static final Map<String, PluginCmd> commands = new HashMap<String, PluginCmd>();
+	static {
+	      commands.put("registerPlugin", new RegisterPluginCmd());
+	      commands.put("unregisterPlugin", new UnregisterPluginCmd());
+	      commands.put("updatePlugin", new UpdatePluginCmd());
+	      commands.put("isPluginRegistered", new IsPluginRegisteredCmd());
+	   }
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -25,7 +41,28 @@ public class GreetingsController {
     }
     
     @RequestMapping(value="/extension", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Extension extension(@RequestBody(required = false) Extension extension) {
+    public Extension extension(@RequestBody(required = false) Extension extension, @RequestParam("action") String action) {
+    	String[] args = new String[14];
+    	args[0] = "-url";
+    	args[1] = System.getProperty("url");
+    	args[2] = "-u";
+    	args[3] = System.getProperty("username");
+    	args[4] = "-p";
+    	args[5] = System.getProperty("password");
+    	args[6] = "-k";
+    	args[7] = extension.getKey();
+    	args[8] = "-v";
+    	args[9] = extension.getVersion();
+    	args[10] = "-action";
+    	args[11] = action;
+    	args[12] = "-pu";
+    	args[13] = extension.getClient().getUrl();
+
+    	for (int i =0; i< args.length; i++) {
+ 		   System.out.println(args[i]);
+ 	   }
+    	PluginCmd pluginCmd = commands.get(action);
+    	pluginCmd.execute(args);
         return extension;
     }
 }
