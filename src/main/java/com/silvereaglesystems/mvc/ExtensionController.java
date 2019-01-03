5f18c8f387be26cddb5b3293c7789536c4b66647
Preston Taukulis <prestontaukulis@Prestons-MacBook-Pro.local>
2019-01-02 20:06:59 -0500

    Cleaning up code

--- src/main/java/com/silvereaglesystems/mvc/ExtensionController.java
+++ src/main/java/com/silvereaglesystems/mvc/ExtensionController.java
@@ -1,9 +1,6 @@
 package com.silvereaglesystems.mvc;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,18 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.vmware.automatic.plugin.registration.PluginRegistrationMain;
+
+import com.silvereaglesystems.mvc.models.Extension;
 import com.vmware.automatic.plugin.registration.commands.IsPluginRegisteredCmd;
 import com.vmware.automatic.plugin.registration.commands.PluginCmd;
 import com.vmware.automatic.plugin.registration.commands.RegisterPluginCmd;
 import com.vmware.automatic.plugin.registration.commands.UnregisterPluginCmd;
 import com.vmware.automatic.plugin.registration.commands.UpdatePluginCmd;
-import com.silvereaglesystems.mvc.models.Client;
-import com.silvereaglesystems.mvc.models.Description;
-import com.silvereaglesystems.mvc.models.Extension;
 
 @RestController
-public class GreetingsController {
+public class ExtensionController {
 	private static final Map<String, PluginCmd> commands = new HashMap<String, PluginCmd>();
 	static {
 	      commands.put("registerPlugin", new RegisterPluginCmd());
@@ -30,19 +25,20 @@ public class GreetingsController {
 	      commands.put("updatePlugin", new UpdatePluginCmd());
 	      commands.put("isPluginRegistered", new IsPluginRegisteredCmd());
 	   }
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
     
     @RequestMapping(value="/extension", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Extension extension(@RequestBody(required = false) Extension extension, @RequestParam("action") String action) {
-    	String[] args = new String[14];
+    	String[] args;
+    	if(action.equals("registerPlugin")) {
+    		args = new String[14];
+    		args[10] = "-v";
+        	args[11] = extension.getVersion();
+        	args[12] = "-pu";
+        	args[13] = extension.getClient().getUrl();
+    	} else {
+    		args = new String[10];
+    	}
+    	 
     	args[0] = "-url";
     	args[1] = System.getProperty("url");
     	args[2] = "-u";
@@ -51,12 +47,8 @@ public class GreetingsController {
     	args[5] = System.getProperty("password");
     	args[6] = "-k";
     	args[7] = extension.getKey();
-    	args[8] = "-v";
-    	args[9] = extension.getVersion();
-    	args[10] = "-action";
-    	args[11] = action;
-    	args[12] = "-pu";
-    	args[13] = extension.getClient().getUrl();
+    	args[8] = "-action";
+    	args[9] = action;
 
     	for (int i =0; i< args.length; i++) {
  		   System.out.println(args[i]);
